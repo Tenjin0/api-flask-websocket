03d6a6fee3b88377523b85aafa696328d837bf81
Patrice <ppetit@wynd.eu>
2018-06-01 10:54:37 +0200

    Core: try websocket and it work

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
 from flask import Flask
-from flask_socketio import SocketIO
+from flask_socketio import SocketIO, emit
 
 app = Flask(__name__, static_folder="static/dist")
 socketio = SocketIO(app)
 
 from routes import index
-import websockets
+
+@socketio.on('connect')
+def connect_handler():
+    print('connected')
+    emit('response', {'meta': 'WS connected'})
+
+@socketio.on('myevent')
+def my_event(msg):
+    print('my event', msg)
+    emit('response', {'meta': 'WS connected'})
 
 if __name__ == "__main__":
-    print(__name__)
     app.run(debug=True)
+
--- static/js/index.js
+++ static/js/index.js
@@ -1,7 +1,13 @@
 import * as io from "socket.io-client";
 
 var socket = io.connect("http://" + document.domain + ":" + location.port);
-socket.on("connect", function() {
-    // we emit a connected message to let knwo the client that we are connected.
-    console.log('Websocket connected!');
+socket.on('connect', function() {
+    console.log('connect')
+    socket.emit('myevent', {
+        data: 'I\'m connected!'
+    });
+});
+
+socket.on('response', function(msg) {
+    console.log('response', msg)
 });
