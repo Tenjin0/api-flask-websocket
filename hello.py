e1eaf9e2432405af3b4d55d7133e3a8970c9cd6f
Tenji <patricepetit@hotmail.com>
2018-06-25 23:00:48 +0200

    CV2: try manipulating cv2

--- hello.py
+++ hello.py
@@ -1,11 +1,29 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
- 
-from Tkinter import * 
+import cv2
+import numpy as np
 
-fenetre = Tk()
 
-label = Label(fenetre, text="Hello World")
-label.pack()
+def create_blank(width, height, rgb_color=(0, 0, 0)):
+    """Create new image(numpy array) filled with certain color in RGB"""
+    # Create black blank image
+    image = np.zeros((height, width, 3), np.uint8)
 
-fenetre.mainloop()
+    # Since OpenCV uses BGR, convert the color first
+    color = tuple(reversed(rgb_color))
+    # Fill image with color
+    print(image)
+    image[:] = color
+    print(image)
+    return image
+
+
+# Create new blank 300x300 red image
+width, height = 2, 2
+
+red = (255, 0, 0)
+image = create_blank(width, height, rgb_color=red)
+# grey = np.zeros((4, 4, 3), np.uint8)
+# print(grey)
+cv2.imshow('ImageWindow', image)
+cv2.waitKey()
--- websockets.py
+++ websockets.py
@@ -3,19 +3,12 @@ import cv2
 import base64
 from flask_socketio import emit
 from app import socketio
-try:
-    import cStringIO as io
-except ImportError:
-    import io
 
-
-camera = cv2.VideoCapture(0)
 img = cv2.imread('static/images/watch.jpg')
 
 """Sends camera images in an infinite loop."""
 
-_, frame = camera.read()
-retval, buffer = cv2.imencode('.jpg', frame)
+retval, buffer = cv2.imencode('.jpg', img)
 # With Pillow
 # sio = io.StringIO()
 # img = Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
@@ -24,8 +17,11 @@ retval, buffer = cv2.imencode('.jpg', frame)
 
 encoded_text = "toto"
 
+
 @socketio.on('connect')
 def connect_handler():
     # encoded_string = base64.b64encode(img)
     # encoded_string = base64.encodestring(encoded_string)
-    emit('camera', {'text':  base64.b64encode(encoded_text), 'buffer': base64.b64encode(buffer)})
+    emit('camera', {'text':  base64.b64encode(encoded_text),
+                    'buffer': "data:image/jpeg;base64," +
+                    base64.b64encode(buffer)})
